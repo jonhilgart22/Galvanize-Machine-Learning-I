c68db9e52890d372364e8c508cf16f523eaa42f8
thompsjj <thompson.jared.79@gmail.com>
2016-04-19 13:39:18 -0700

    logistic regression solutions

---
+++ week3/3.2/logistic_regression_practicum_solutions/logistic_regression/roc_curve.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def roc_curve(probabilities, labels):
+    '''
+    INPUT: numpy array, numpy array
+    OUTPUT: list, list, list
+    Take a numpy array of the predicted probabilities and a numpy array of the
+    true labels.
+    Return the True Positive Rates, False Positive Rates and Thresholds for the
+    ROC curve.
+    '''
+    
+    thresholds = np.sort(probabilities)
+
+    tprs = []
+    fprs = []
+
+    num_positive_cases = sum(labels)
+    num_negative_cases = len(labels) - num_positive_cases
+
+    for threshold in thresholds:
+        # With this threshold, give the prediction of each instance
+        predicted_positive = probabilities >= threshold
+        # Calculate the number of correctly predicted positive cases
+        true_positives = np.sum(predicted_positive * labels)
+        # Calculate the number of incorrectly predicted positive cases
+        false_positives = np.sum(predicted_positive) - true_positives
+        # Calculate the True Positive Rate
+        tpr = true_positives / float(num_positive_cases)
+        # Calculate the False Positive Rate
+        fpr = false_positives / float(num_negative_cases)
+
+        fprs.append(fpr)
+        tprs.append(tpr)
+
+    return tprs, fprs, thresholds.tolist()
+
