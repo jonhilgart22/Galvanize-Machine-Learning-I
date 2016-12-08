bd767d961b1ef0cf4f3ed259e0286e559d8e7dce
jonathanhilgart <jonathan.hilgart@gmail.com>
2016-12-08 13:05:32 -0800

    week 7 7.3

--- Final_Project_Ultimatum_Game/model_testing.py
+++ Final_Project_Ultimatum_Game/model_testing.py
@@ -14,17 +14,14 @@ from sklearn.model_selection import RandomizedSearchCV
 
 class Model_Testing_Regression():
 	"""Test a number of different machine learning regression algorithmns on your data. Need to manually optimize, no gridsearch."""
-
 	def __init__(self,X,y,number_of_folds,x_labels,y_label):
 		"""Initialize the class with the data and train test split"""
-
 		self.X=X
 		self.y=y
 		self.number_of_folds=number_of_folds
 		self.X_trainval, self.X_test, self.y_trainval, self.y_test = train_test_split(X,y,test_size=.2) ##smaller test size due to less data
 		self.x_labels =x_labels
 		self.y_label = y_label
-
 	def random_forest(self, number_estimators=10,n_features='auto', m_depth=None):
 		"""Given the parameters, return the RMSE, accuracy, and feature importance.
 		Return str(rmse val), rmse val, str(rmse train),rmse train, str(rmse test), rmse test, feature importance and weight"""
@@ -88,12 +85,9 @@ class Model_Testing_Regression():
 		sorted_features = self.x_labels[np.argsort(feature_importances_final)[::-1]]
 		return 'RMSE Val:',np.mean(rmse_val),'RMSE Train:',np.mean(rmse_train),'RMSE TEST:',np.mean(rmse_test), [(feature,weight)\
 		 for feature,weight in zip(sorted_features,feature_importances_final[np.argsort(feature_importances_final)[::-1]])]
-
 	def extreme_gradient_boost(self,max_depth_n=3,learning_rate_n=.1,n_estimators_n=100,reg_alpha_n=0,reg_lambda_n=0):
 		"""Perform extreme gradient boosting on the given data. 
 		Returns str(rmse val), rmse val, str(rmse train),rmse train, str(rmse test), rmse test, feature importance and weight """
-
-
 		#######Attributes###########
 		#xgboost.XGBRegressor(max_depth=3, learning_rate=0.1, n_estimators=100, silent=True,
 		 #objective='reg:linear', nthread=-1, gamma=0, min_child_weight=1, max_delta_step=0, 
@@ -123,15 +117,12 @@ class Model_Testing_Regression():
 		sorted_features = self.x_labels[np.argsort(feature_importances_final)[::-1]]
 		return 'RMSE Val:',np.mean(rmse_val),'RMSE Train:',np.mean(rmse_train),'RMSE TEST:',np.mean(rmse_test), [(feature,weight)\
 		 for feature,weight in zip(sorted_features,feature_importances_final[np.argsort(feature_importances_final)[::-1]])]
-
-
 	def glm_net(self,alpha_n=1.0,l1_ratio_n=.5,normalize_f=False):
 		"""Use GLM net with the given parameters to produce a regression model.
 		Returns str(rmse val), rmse val, str(rmse train),rmse train, str(rmse test), rmse test, feature importance and weight """
 		########Attributes########
 		#ElasticNet(alpha=1.0, l1_ratio=0.5, fit_intercept=True, normalize=False, 
-		#	precompute=False, max_iter=1000, copy_X=True, tol=0.0001, warm_start=False, positive=False, random_state=None, selection='cyclic')
-		
+		#	precompute=False, max_iter=1000, copy_X=True, tol=0.0001, warm_start=False, positive=False, random_state=None, selection='cyclic')	
 		rmse_val = []
 		rmse_train = []
 		rmse_test = []
@@ -156,32 +147,52 @@ class Model_Testing_Regression():
 		sorted_features = self.x_labels[np.argsort(feature_importances_final)[::-1]]
 		return 'RMSE Val:',np.mean(rmse_val),'RMSE Train:',np.mean(rmse_train),'RMSE TEST:',np.mean(rmse_test), [(feature,weight)\
 		 for feature,weight in zip(sorted_features,feature_importances_final[np.argsort(feature_importances_final)[::-1]])]
-
 	def random_grid_search(self,model=None,params_dict=None,iterations=100,cv_n=3):
-		"""params is a dict of the parameters for the model. Return the best model parameters. Model is text for the model selection."""
-
-		if model = 'glm_net':
-
-			random_search = RandomizedSearchCV(estimator=self.glm_net(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
-			random_search.fit(self.X_trainval,self,y_trainval)
+		"""params is a dict of the parameters for the model. Return the best model parameters. 
+		Model is text for the model selection (glm_net,extreme_gradient_boost,gradient_boost,random_forest.
+		Train on the a training data."""
+		if model == 'glm_net':
+			random_search = RandomizedSearchCV(estimator=ElasticNet(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
+			random_search.fit(self.X_trainval,self.y_trainval)
+			#print(random_search.best_estimator_)
+			self.best_random_forest = random_search.best_estimator_ ## save our best performing estimator
 			return random_search.best_estimator_
-		elif model = 'extreme_gradient_boost':
-
-			random_search = RandomizedSearchCV(estimator=self.extreme_gradient_boost(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
-			random_search.fit(self.X_trainval,self,y_trainval)
+		elif model == 'extreme_gradient_boost':
+			random_search = RandomizedSearchCV(estimator=xgboost.XGBRegressor(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
+			random_search.fit(self.X_trainval,self.y_trainval)
+			self.best_extreme_gradient_boost = random_search.best_estimator_ ## save the best performing estimator
 			return random_search.best_estimator_
-
-		elif model = 'gradient_boost':
-
-			random_search = RandomizedSearchCV(estimator=self.gradient_boost(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
-			random_search.fit(self.X_trainval,self,y_trainval)
+		elif model == 'gradient_boost':
+			random_search = RandomizedSearchCV(estimator= GradientBoostingRegressor(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
+			random_search.fit(self.X_trainval,self.y_trainval)
+			self.best_gradient_boost = random_search.best_estimator_
 			return random_search.best_estimator_
-
-		elif model = 'random_forest':
-
-			random_search = RandomizedSearchCV(estimator=self.random_forest(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
-			random_search.fit(self.X_trainval,self,y_trainval)
+		elif model == 'random_forest':
+			random_search = RandomizedSearchCV(estimator=RandomForestRegressor(),param_distributions=params_dict,cv=cv_n,n_iter=iterations,verbose=1)
+			random_search.fit(self.X_trainval,self.y_trainval)
+			self.best_random_forest = random_search.best_estimator_
 			return random_search.best_estimator_
+		else:
+			print('There is no model by that name.')
+
+	def predict(self,model=None):
+		"""Create predictions on the test data given the model."""
+		if model == 'glm_net':
+			self.best_random_forest.fit(self.X_trainval,self.y_trainval)
+			return self.best_random_forest.predict(self.X_test)## predictions
+		elif model == 'extreme_gradient_boost':
+			self.best_extreme_gradient_boost.fit(self.X_trainval,self.y_trainval)
+			return self.best_extreme_gradient_boost.predict(self.X_test)
+		elif model == 'gradient_boost':
+			self.best_gradient_boost.fit(self.X_trainval,self.y_trainval)
+			return self.best_gradient_boost.predict(self.X_test)
+		elif model == 'random_forest':
+			self.best_random_forest.fit(self.X_trainval,self.y_trainval)
+			return self.best_random_forest.predict(self.X_test)
+		else:
+			print('There is no model by that name.')
+
+
 
 
 
